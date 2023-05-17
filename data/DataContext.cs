e0175984a25d6eb8b22b0b3692247812abda8cac
Andynajera <andrenaje94@hotmail.com>
2023-05-17 12:22:04 +0200

    dataCambios

---
+++ data/DataContext.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using Classes;
+
+namespace Data
+{
+
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+       
+        public DbSet<User>? User { get; set; }
+        public DbSet<Degree>? Degrees { get; set; }
+        public DbSet<Pago>? Pagos { get; set; }
+        public DbSet<OrderPro>? OrderPros { get; set; }
+
+    }
+}
